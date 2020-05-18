0f0c5d57ab7f64791aa170d2a25504066f2edfdb
Ivan <ivan@plugion.org>
2020-05-18 12:33:41 +0300

    Add files via upload

---
+++ wbk_service_quantity_filter.php
@@ -0,0 +1,35 @@
+<?php
+
+add_filter( 'wbk_service_quantity', 'my_service_quantity', 10, 3 );
+function my_service_quantity( $input, $service_id, $time) {
+	$weekday = date('N', $time );
+	switch ( $weekday ) {
+		case '1':
+			return 2;
+			break;
+		case '2':
+			return 2;
+			break;	
+		case '3':
+			return 4;
+			break;
+		case '4':
+			return 4;
+			break;
+		case '5':
+			return 2;
+			break;
+		case '6':
+			return 4;
+			break;
+		case '7':
+			return 2;
+			break;
+		default:
+			return 2;
+			break;
+	}
+ 
+}
+
+?>
